6c05b1d1318108c6e6eba388d84de1157b927c9d
tsuriyathep <tsuriyathep@users.noreply.github.com>
2022-10-11 16:56:46 -0700

    3 files modified, 1 added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,56 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed, watch } from 'vue';
 
-defineProps({
-  msg: String
-})
+let symbolsInOrder = ref('Wild,H1,H2,H3,H4,H5,L1,L2,L3,L4,L5,L6,Bonus,Replace');
+let reelText = ref(`Bonus	Bonus	Bonus	Bonus	Bonus
+L3	L3	L2	L4	L5
+H2	L3	H2	H2	H2`);
+let reelResult = ref('');
 
-const count = ref(0)
+function process() {
+  let symsArr = [];
+
+  if (symbolsInOrder.value.trim().length > 0) {
+    symsArr = symbolsInOrder.value
+      .toUpperCase()
+      .trim()
+      .split(',')
+      .map((n) => n.trim());
+  }
+
+  let reelsStr = reelText.value.trim().toUpperCase();
+  let rows = reelsStr.split('\n');
+  let cols = rows[0].trim().split('\t').length;
+  let arr = [];
+  for (let j = 0; j < cols; j++) {
+    let colSymbols = [];
+    for (let i = 0; i < rows.length; i++) {
+      let vals = rows[i].split('\t');
+      let sym = vals[j].toUpperCase();
+      if (sym.length == 0) continue;
+      let index = symsArr.indexOf(sym);
+      if (index > -1) sym = index;
+      colSymbols.push(sym);
+    }
+    arr.push(colSymbols);
+  }
+  reelResult.value = JSON.stringify(arr);
+}
+
+watch([symbolsInOrder, reelText], (newValue, oldValue) => {
+  process();
+});
+
+process();
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <b>SYMBOLS IN ORDER (comma separated):</b>
+  <textarea v-model="symbolsInOrder" style="width: 100%" rows="3" />
+  <b>INPUT (from Excel tab separated):</b>
+  <textarea v-model="reelText" style="width: 100%" rows="20" />
+  <b>OUTPUT (JSON array):</b>
+  <textarea v-model="reelResult" style="width: 100%" rows="15" />
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
